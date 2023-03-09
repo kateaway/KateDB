31d829f1d42855047273ce1f8b11ca6499e0bb04
kateaway <katya.040521@mail.ru>
2023-03-09 21:23:19 +0300

    fg

---
+++ main.py
@@ -0,0 +1,25 @@
+import asyncio
+
+# A fast co-routine
+async def add_(x, y):
+    print("starting fast add")
+    await asyncio.sleep(3) # Mimic some network delay
+    print("result ready for fast add")
+
+# A slow co-routine
+async def add_slow(x, y):
+    print("starting slow add")
+    await asyncio.sleep(5) # # Mimic some network delay
+    print("result ready for slow add")
+
+
+# Create a function to schedule co-routines
+async def get_results():
+    task1 = asyncio.create_task(add_slow(3, 4))
+    task2 = asyncio.create_task(add_fast(5, 5))
+
+    await task1
+    await task2
+    print('done') # Prints 7 10
+
+asyncio.run(get_results())
